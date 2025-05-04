325420b5c722df30d72779fd18d311720ed2470e
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-05-04 20:16:03 +0100

    add plaintext component

---
+++ src/promptworks/interfaces.py
@@ -0,0 +1,45 @@
+from typing import Protocol, runtime_checkable
+from lxml.etree import _Element
+
+
+@runtime_checkable
+class BasePromptComponent(Protocol):
+    """
+    Interface for a prompt component.
+    """
+
+    def as_xml(self) -> _Element:
+        """
+        Convert the component to an XML string.
+
+        This should be used to render the component as xml, do not fetch data or do computation here.
+        """
+        ...
+
+    def as_json(self) -> dict:
+        """
+        Convert the component to a JSON string.
+
+        This should be used to render the component as json, do not fetch data or do computation here.
+        """
+        ...
+
+@runtime_checkable
+class AsyncRefreshable(Protocol):
+    """
+    Interface for a component that can be refreshed asynchronously.
+    """
+
+    async def refresh(self) -> None:
+        """
+        Refresh the component.
+
+        This will be called every time the component is rendered, and should be used to fetch the latest data.
+        """
+
+@runtime_checkable
+class DynamicPromptComponent(BasePromptComponent, AsyncRefreshable, Protocol):
+    """
+    Interface for a dynamic prompt component.
+    """
+    ...
---
+++ tests/test_compontents_plaintext.py
@@ -0,0 +1,16 @@
+from promptworks.components.plaintext import PlaintextComponent
+
+
+def test_plaintext_component():
+    component = PlaintextComponent("test", "Hello, world!")
+    assert component.name == "test"
+    assert component.text == "Hello, world!"
+
+    xml = component.as_xml()
+    assert xml.tag == "test"
+    assert xml.text == "Hello, world!"
+
+    json = component.as_json()
+    assert json["type"] == "plaintext"
+    assert json["name"] == "test"
+    assert json["text"] == "Hello, world!"
