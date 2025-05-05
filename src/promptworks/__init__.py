6714027f58940617127656538707cec9615ee411
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-05-05 13:09:45 +0100

    add proper public interface

--- src/promptworks/__init__.py
+++ src/promptworks/__init__.py
@@ -1,2 +1,19 @@
+from promptworks.components import (
+    LocalFileComponent,
+    TimeComponent,
+    PlaintextComponent,
+)
+
+from promptworks.prompt import Prompt
+from promptworks import interfaces
+
+__all__ = [
+    "LocalFileComponent",
+    "TimeComponent",
+    "PlaintextComponent",
+    "Prompt",
+    "interfaces",
+]
+
 def main() -> None: # pragma: no cover
     print("Hello from promptworks!")
